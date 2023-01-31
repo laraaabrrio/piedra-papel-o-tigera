ca0841813a76f34b511f34ee8d33daa101484ce1
laraaabrrio <122534361+laraaabrrio@users.noreply.github.com>
2023-01-31 13:48:10 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,68 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    while (enviado == 0) {
+        basic.pause(100)
+    }
+    if (receivedNumber == 111) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+    if (receivedNumber == 222) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    }
+    if (receivedNumber == 333) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+    radio.sendNumber(111)
+    enviado = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    radio.sendNumber(333)
+    enviado = 1
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    radio.sendNumber(222)
+    enviado = 1
+})
+let enviado = 0
+radio.setGroup(111)
+enviado = 0
